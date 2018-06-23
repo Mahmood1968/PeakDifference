fee80d6375846033ae78a00e3eef2771689c4a0e
Mahmood1968 <Mahmood_abdullah@hotmail.com>
2018-06-23 19:15:31 -0400

    Add files via upload

---
+++ MiniProject/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace MiniProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 5;
+            this.numericUpDown1.Location = new System.Drawing.Point(1150, 652);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            866,
+            0,
+            0,
+            327680});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1338, 652);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1452, 652);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.MaximumSize = new System.Drawing.Size(2024, 600);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Data";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1522, 600);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(951, 659);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Difference";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(948, 643);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Difference";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(641, 654);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Peak1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(638, 638);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Peak1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(801, 655);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "peak2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(798, 638);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Peak2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(504, 654);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "factor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(501, 638);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "factor";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(379, 652);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 22);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1338, 623);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1452, 623);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Restart";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1562, 687);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "MiniProject";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ MiniProject/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections; 
+
+namespace MiniProject
+{
+
+    
+
+/*=====================================================================*/
+    public partial class Form1 : Form
+    {
+        public double factor=0.0086; 
+        ReadDataFile reading=new ReadDataFile();
+        private int clickCount = 0;
+        string FileName; 
+       
+        public Form1()
+        {
+            InitializeComponent();
+          //  this.textBox1.Text= "Difference";
+            this.Text = "DATA ANALYSIS THE DIFFERENCE BETWEEN PEAKS ";
+
+        }
+/*==============================THIS IS PROCESS BUTTON =======================*/
+     private void button1_Click(object sender, EventArgs e)
+        {
+            
+                   
+               clickCount++; 
+               reading.Readtext3(FileName);
+               /*=======================================================*/
+                string f = this.textBox4.Text.ToString();
+
+                double factor =Convert.ToDouble(this.numericUpDown1.Value.ToString()); 
+                int p1 = reading.setMinPeak();
+                int p2 = reading.setMaxPeak();
+                this.textBox2.Text = p1.ToString();// peak1.ToString();
+                this.textBox3.Text = p2.ToString();
+                this.textBox4.Text = factor.ToString(); 
+
+                double Difference = reading.Peaksdifference(factor);
+                this.textBox1.Text = Difference.ToString();
+
+               
+                    reading.WriteInExcel(Difference, clickCount);    //Column 
+                    Console.WriteLine("This file {0}", clickCount);
+                    reading.Bank0Data.Clear(); // New added 
+               
+             //******************************************************* 
+              DrawChart(reading.BankAll.ToArray());
+             
+            
+        }
+
+  
+ 
+   private void DrawChart(int[] DataIn)
+            {
+
+                chart1.Series.Clear();
+                Refresh();
+
+                this.chart1.ChartAreas[0].AxisY.Crossing = 0;
+
+                chart1.ChartAreas[0].AxisX.Title = "Values";
+                chart1.ChartAreas[0].AxisX.Maximum = DataIn.Length;
+                chart1.ChartAreas[0].AxisX.Minimum = 0;
+                chart1.ChartAreas[0].AxisX.Interval = 42;
+                chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+                chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+                chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = true;
+                if (chart1.Series.IsUniqueName("Data"))
+                {
+                    chart1.Series.Add("Data");
+                }
+              
+                chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                for (int i = 0; i <= DataIn.Length - 1; i++)
+                    this.chart1.Series["Data"].Points.AddXY(i, DataIn[i]);
+
+
+
+            }
+   /*==============================THIS IS CLOSE BUTTON =======================*/
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+             Random rnd = new Random();
+            string nChartImage = "chart1" + rnd.Next(1, 13).ToString()+".png";
+
+            this.chart1.SaveImage(nChartImage, ChartImageFormat.Png); 
+        }
+  /*==============================THIS IS LOAD BUTTON =======================*/
+        private void button4_Click(object sender, EventArgs e)
+        {
+         string Pathdata; 
+         System.Windows.Forms.OpenFileDialog openFileDialog1;
+         openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+         openFileDialog1 = new OpenFileDialog();
+         openFileDialog1.ShowDialog();
+         openFileDialog1.Title = "Load  Data File";
+         openFileDialog1.DefaultExt = "txt";
+         Pathdata = openFileDialog1.FileName;
+         FileName = Path.GetFileName(Pathdata);
+         Console.WriteLine("the file name is ={0}", FileName); 
+
+        }
+    /*==============================THIS IS RESTART BUTTON =======================*/
+        private void button5_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            Refresh();
+            reading.Bank0Data.Clear();
+            reading.BankAll.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            clickCount = 0;  
+
+        }
+
+    }
+    /*==========================================================================================
+     * ReadDataFile Class reads that data from Text files , Save data in Excel Sheet 1 and Sheet2
+     * Calculates the minimum and maximum data  peaks and the difference between them.
+     * 
+     * *****************************************************************************************/
+    class ReadDataFile
+    {
+        public List<int> Bank0Data = new List<int>();
+        public List<int> BankAll = new List<int>();
+        int peak1, peak2;
+        int addSheet = 0;
+        
+  /*======================================================*/
+        public int setMaxPeak()
+        {
+            peak1 = -1; 
+            int maxIndex = -1;
+            double maxInt = Int32.MinValue;
+             
+            
+            for (int i = 0; i < BankAll.Count; i++)
+            {
+                int value = BankAll[i];
+                if (value > maxInt)
+                {
+                    maxInt = value;
+                    maxIndex = i;
+                }
+            }
+            peak1 = maxIndex;
+            return peak1;
+        }
+        /*======================================================*/
+        public int setMinPeak()
+        {
+            peak2 = -1; 
+            int minIndex = -1;
+            double minInt = Convert.ToDouble(Int32.MaxValue);
+            
+            
+            for (int i = 0; i < BankAll.Count; i++)
+            {
+                int value = BankAll[i];
+                if (value < minInt)
+                {
+                    minInt = value;
+                    minIndex = i;
+                }
+            }
+            peak2 = minIndex;
+            return peak2;
+        }
+        /*======================================================*/
+        public double Peaksdifference(double factor)
+        {
+            return ((double)peak2 - (double)peak1) * factor;
+        }
+
+        /*======================================================*/
+        public void Readtext3(string fileName)
+        {
+            string fileContent1 = File.ReadAllText(fileName);
+            string[] integerStrings1 = fileContent1.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int Maxlength = integerStrings1.Length;
+            int val;
+            for (int n = 0; n < Maxlength; n++)
+            {
+                val = int.Parse(integerStrings1[n]);
+                Bank0Data.Add(val);
+                BankAll.Add(val); 
+                Console.WriteLine("{0} \t {1} ", n, Bank0Data[n]);
+            }
+
+            Console.WriteLine("Number of elements (rows) is ={0}", Bank0Data.Count);
+            Console.WriteLine(" Data type of {0} ", Bank0Data.GetType());
+
+          
+
+        }
+
+
+
+        public void WriteInExcel(double Difference, int column)
+        {
+
+            string filename = "File" + column.ToString();
+            Console.WriteLine("1- The size of this array={0}", Bank0Data.Count.ToString());
+            var spreadsheetLocation = Path.Combine(Directory.GetCurrentDirectory(), "sdata_test.xlsx");
+            var exApplication = new Microsoft.Office.Interop.Excel.Application();
+            var exWorkbook = exApplication.Workbooks.Open(spreadsheetLocation);
+           // var   exWorksheet = exWorkbook.Sheets.Add(After: exWorkbook.Sheets[1]);
+
+            var exWorksheet = (Excel.Worksheet)exWorkbook.Worksheets.get_Item(1);
+            exWorksheet.Cells[column, 1] = Difference;
+            if (addSheet == 0)
+            {
+                exWorksheet = exWorkbook.Sheets.Add(After: exWorkbook.Sheets[1]);
+                addSheet++; 
+            }
+            else exWorksheet = (Excel.Worksheet)exWorkbook.Worksheets.get_Item(2);
+
+            for (var row = 2; row <= Bank0Data.Count - 1; row++)
+            {
+                var cell = (Excel.Range)exWorksheet.Cells[row, column];
+                cell.Value2 = Bank0Data[row];
+            }
+            var header = (Excel.Range)exWorksheet.Cells[1, column];
+            header.Value2 = "File"+column;
+
+            Console.WriteLine("2- The size of this array={0}", Bank0Data.Count.ToString());
+            exWorkbook.Save();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+           //  Marshal.ReleaseComObject(writeRange);
+            Marshal.ReleaseComObject(exWorksheet);
+            //close and release and  
+            //quit and release
+
+            exApplication.Quit();
+            Marshal.ReleaseComObject(exApplication);
+            //Console.WriteLine("3- The size of this array={0}", Bank0Data.Count.ToString());
+
+        }
+
+
+    }   // END OF THE CLASS    // 
+    /*======================================================================*/
+}
